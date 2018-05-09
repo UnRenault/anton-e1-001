4af3b4d603b0c633c5005f89bd68e1e1cd35149f
UnRenault <fackinsheed@gmail.com>
2018-05-09 18:07:17 +0300

    домашка_1

---
+++ 2lesson/common.js
@@ -0,0 +1,31 @@
+// const a = +prompt ('укажите a');
+// const b = +prompt ('укажите b');
+// alert(2*(a+b));
+
+// let counter = 0
+// while (counter <= 50) {
+// 	console.log (`The number is ${counter}`);
+// 	counter++
+// }
+
+// for (let i=1; i<=10; ++i) {
+// 	let z= i*2;
+// 	console.log(`${i} * 2 = ${z}`);
+// }
+
+// let num = 1;
+
+// while (num <= 10) {
+//  z = num*2;
+//  console.log(`${num} * 2 = ${z}`);
+//  num++
+// }
+
+let i = 1;
+
+do  {
+  let p = i*2;
+  console.log (`${i}*2=${p}`);
+  i++;
+}
+  while (i<=10);
---
+++ lesson_1/js/common.js
@@ -0,0 +1,15 @@
+// const randomNumber = Math.random();
+
+// const string = `Мое случайное число ${randomNumber}`;
+
+// const day = 29;
+
+// const month = `март`;
+
+// const year = 1994;
+
+// const all = `Дата моего рождения ${day} ${month} ${year}`;
+
+const number = '20';
+
+const result = `Результат сложения числа ${+number+100}`;
